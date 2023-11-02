0d92448b73370db17748e39ed585f020b196a446
Siranut Akarawuthi <58118786+pkbsa@users.noreply.github.com>
2023-11-02 13:50:00 +0700

    Update DirectionTest.java

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -4,12 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * A very simple (and not particularly useful)
- * test class to have a starting point where to put tests.
- *
- * @author Arie van Deursen
- */
+ /**
+     * DirectionTest Class
+*/
 public class DirectionTest {
     /**
      * Do we get the correct delta when moving north?
