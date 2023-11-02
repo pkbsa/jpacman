018e8c6e0c6a5b7a70d823d72aab67b9eca1a67b
Siranut Akarawuthi <58118786+pkbsa@users.noreply.github.com>
2023-11-02 13:49:06 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+     * BoardTest Class.
+*/
 public class BoardTest {
+    /**
+     * testBoard Fucntion.
+    */
     @Test
     void testBoard() {
         Square s1 = new BasicSquare();
@@ -14,12 +20,15 @@ public class BoardTest {
         assertThat(board.invariant()).isTrue();
     }
 
+    /**
+     * testBoardOneNullSquare Fucntion.
+    */
     @Test
-    void testBoardOneNullSquare(){
+    void testBoardOneNullSquare() {
         Square s1 = new BasicSquare();
         Square[][] grid = new Square[1][1];
         grid[0][0] = s1;
         Board board = new Board(grid);
-        assertThat(board.squareAt(0,0)).isEqualTo(grid[0][0]);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
     }
 }
